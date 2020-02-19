0bdf536df62480d29e5075eca86440294b3ab73d
Josiah <reikiryo@yahoo.com>
2020-02-19 10:09:40 -0500

    mapped state to props

---
+++ src/reducers/featureReducers.js
@@ -0,0 +1,17 @@
+const initialState = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image: 'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    }
+};
+
+export const featureReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
+
